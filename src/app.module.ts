66c2d4a248341f334ee7013ba4e372234c08927b
Daniel Durán <danielduranje@outlook.com>
2023-06-12 15:49:07 -0500

    estructura ampliada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -14,7 +17,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+    ProductsModule,
+    CommonModule,
+    SeedModule
   ],
 })
 export class AppModule {}
